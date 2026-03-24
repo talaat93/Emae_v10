e95749f31aa9e51a3000e0b45ca232cb8d517215
talaat93 <61964222+talaat93@users.noreply.github.com>
2026-03-24 20:38:47 +0100

    Delete emae_site_v4 directory

--- emae_site_v4/includes/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/db.php';
-require_once __DIR__ . '/functions.php';
-
-initializeDatabase();
--- emae_site_v4/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/bootstrap.php';
-unset($_SESSION['user_id']);
-flash('success', 'Vous êtes déconnecté.');
-redirect('index.php');
